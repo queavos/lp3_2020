408ba93562b8868f9cc237773bee26469300e1c3
Ozba Micniuk <gt.ozba@gmail.com>
2020-04-21 11:25:58 -0400

    spring clase 5

---
+++ spring2/src/main/java/unae/lp3/controller/HomeController.java
@@ -0,0 +1,19 @@
+package unae.lp3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="home", method=RequestMethod.GET) 
+	public String index()
+	{		
+		return "index";
+	}
+	@RequestMapping(value="dos", method=RequestMethod.GET) 
+	public String dos()
+	{		
+		return "dos";
+	}
+}
